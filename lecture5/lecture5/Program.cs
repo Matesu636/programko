bd6a8326ce0faeac9b68e5837508334a250c67e4
Matesu636 <145652231+Matesu636@users.noreply.github.com>
2023-11-02 09:32:34 +0100

    Add files via upload

---
+++ lecture5/lecture5/Program.cs
@@ -0,0 +1,55 @@
+﻿//int PlusJedna(int x)
+//{
+//    return x + 1;
+//}
+//int x = 3;
+
+//int y = PlusJedna(x);
+
+//Console.WriteLine(y);
+
+//float maximum(float x,float y)
+//{
+//    if (x > y)
+//    {
+//        return x;
+//    }
+
+//    else
+//    {
+//        return y;  
+//    }
+//}
+
+//float x = 0;   
+//float y = 9;
+//Console.WriteLine(maximum(x,y));
+
+//bool aresame(float x, float y)
+//{
+//    if (x == y)
+//    {
+//        return true;
+//    }
+
+//    else
+//    {
+//        return false;
+//    }
+//}
+
+//float x = 7;
+//float y = 7;
+//Console.WriteLine(aresame(x, y));
+
+Console.WriteLine("Napis cislo");
+int CisloOdUyivatele()
+{
+    string input = Console.ReadLine();
+    int Intcislo;
+    int.TryParse(input, out Intcislo);
+    return Intcislo;
+}
+
+
+CisloOdUyivatele(); 
