7ccc8305c2507407a12ad3c1a7b4b70960470ebe
Matesu636 <145652231+Matesu636@users.noreply.github.com>
2023-11-13 14:34:44 +0100

    Add files via upload

---
+++ kvadraticka rovnice ukol/kvadraticka rovnice ukol/kvadraticka rovnice ukol/Program.cs
@@ -0,0 +1,30 @@
+﻿double koeficient(string koef)
+{
+    while (true)
+    {
+        Console.WriteLine("Napis koeficient "+ koef);
+        string  cislo= Console.ReadLine(); 
+        double.TryParse(cislo, out double cislo1);
+        if (double.TryParse(cislo, out  cislo1))
+        {
+            return cislo1;
+        }
+        else
+        {
+            Console.WriteLine("Neni to cislo");
+        }
+    }
+}
+
+double a = koeficient("a");
+double b = koeficient("b");
+double c = koeficient("c");
+
+
+Console.WriteLine("Rovnice je " + a + "*x*x + " + b + "*x *+"  + c);
+
+double diskriminant = b * b - 4 * a * c;
+if (diskriminant < 0)
+    {
+    Console.WriteLine("Neni reseni");
+    }
