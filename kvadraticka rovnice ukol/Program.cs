f7183f1919fe3b3835de449a17e1213801e86f17
Matesu636 <145652231+Matesu636@users.noreply.github.com>
2023-11-13 10:36:21 +0100

    Add files via upload

---
+++ kvadraticka rovnice ukol/Program.cs
@@ -0,0 +1,21 @@
+﻿double koeficient(string koef)
+{
+    while (true)
+    {
+        Console.WriteLine("Napis koeficient "+ koef);
+        string  cislo= Console.ReadLine(); 
+        double.TryParse(cislo, out double x);
+        if (true)
+        {
+            Console.WriteLine("Neni to cislo");
+        }
+        else
+        {
+            break;
+        }
+    }
+}
+
+double a = koeficient("a");
+double b = koeficient("b");
+double c = koeficient("c");
